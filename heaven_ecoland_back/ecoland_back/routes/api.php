61bb14b0c8731bebf07dfaad8fa8bc3ca0c0e14f
raed <testuser@example.com>
2019-09-22 01:14:47 +0300

    bundles

--- heaven_ecoland_back/ecoland_back/routes/api.php
+++ heaven_ecoland_back/ecoland_back/routes/api.php
@@ -16,3 +16,14 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// List bundles
+Route::get('bundles', 'bundleController@index');
+// List single article
+Route::get('article/{id}', 'bundleController@show');
+// Create new article
+Route::post('bundle', 'bundleController@store');
+// Update article
+Route::put('article', 'bundleController@store');
+// Delete article
+Route::delete('article/{id}', 'bundleController@destroy');
