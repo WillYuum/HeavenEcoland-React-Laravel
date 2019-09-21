c5d2b492844f15beb371ae4ba618dc5fc2a66e37
raed <testuser@example.com>
2019-09-22 00:47:32 +0300

    create connnection

---
+++ heaven_ecoland_back/ecoland_back/app/bundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class bundle extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'bundle';
+}
