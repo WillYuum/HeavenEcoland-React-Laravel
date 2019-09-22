bc2b099cf4fc1d19aa7ed0ac4ccc2dd704d7b0f3
raed <testuser@example.com>
2019-09-22 09:05:43 +0300

    tables

---
+++ heaven_ecoland_back/ecoland_back/database/migrations/2019_09_22_055527_add_columns_to_bundle.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsToBundle extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('bundle', function (Blueprint $table) {
+            // add columns
+            $table->string('name', 255)->nullable();
+            $table->bigInteger('bundle_id')->nullable();
+            $table->bigInteger('price')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('bundle', function (Blueprint $table) {
+            //drop columns
+            // $table->dropColumn('paid');
+        });
+    }
+}
