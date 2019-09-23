6b38b6e1fd8011b29c2ca94310b41f7aaa76e21a
William <liams_1998@hotmail.com>
2019-09-23 12:52:29 +0300

    added router

--- heaven_ecoland_front/src/App.js
+++ heaven_ecoland_front/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Route, Link, Switch, withRouter } from "react-router-dom";
 
 //-------------CSS---------------
 import "./App.css";
@@ -8,14 +9,109 @@ import "./App.css";
 import Nav from "./Components/Nav/Nav.js";
 import Footer from "./Components/footer/Footer.js";
 import LandingPage from "./Pages/LandingPage/LandingPage.js";
+import GalleryPage from "./Pages/GalleryPage/GalleryPage";
+import EventPage from "./Pages/EventPage/EventPage.js";
+import BlogPage from "./Pages/BlogPage/BlogPage.js";
 //---------IMPORTED COMPONENTS--------------
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      galleryData: [],
-      eventsData: [],
+      galleryData: [
+        {
+          img:
+            "https://lh3.googleusercontent.com/bCDiJJkItiG4-s1ijDXxtsKy60hpf4Jo1fiQExKujtORmIrWgczZ67gLmFifzUuDxaRbaVV1oYgGD4HE4g=w768-h768-n-o-v1"
+        },
+        {
+          img:
+            "https://lh3.googleusercontent.com/bCDiJJkItiG4-s1ijDXxtsKy60hpf4Jo1fiQExKujtORmIrWgczZ67gLmFifzUuDxaRbaVV1oYgGD4HE4g=w768-h768-n-o-v1"
+        },
+        {
+          img:
+            "https://lh3.googleusercontent.com/bCDiJJkItiG4-s1ijDXxtsKy60hpf4Jo1fiQExKujtORmIrWgczZ67gLmFifzUuDxaRbaVV1oYgGD4HE4g=w768-h768-n-o-v1"
+        },
+        {
+          img:
+            "https://lh3.googleusercontent.com/bCDiJJkItiG4-s1ijDXxtsKy60hpf4Jo1fiQExKujtORmIrWgczZ67gLmFifzUuDxaRbaVV1oYgGD4HE4g=w768-h768-n-o-v1"
+        },
+        {
+          img:
+            "https://lh3.googleusercontent.com/bCDiJJkItiG4-s1ijDXxtsKy60hpf4Jo1fiQExKujtORmIrWgczZ67gLmFifzUuDxaRbaVV1oYgGD4HE4g=w768-h768-n-o-v1"
+        },
+        {
+          img:
+            "https://lh3.googleusercontent.com/bCDiJJkItiG4-s1ijDXxtsKy60hpf4Jo1fiQExKujtORmIrWgczZ67gLmFifzUuDxaRbaVV1oYgGD4HE4g=w768-h768-n-o-v1"
+        },
+        {
+          img:
+            "https://lh3.googleusercontent.com/bCDiJJkItiG4-s1ijDXxtsKy60hpf4Jo1fiQExKujtORmIrWgczZ67gLmFifzUuDxaRbaVV1oYgGD4HE4g=w768-h768-n-o-v1"
+        },
+        {
+          img:
+            "https://lh3.googleusercontent.com/bCDiJJkItiG4-s1ijDXxtsKy60hpf4Jo1fiQExKujtORmIrWgczZ67gLmFifzUuDxaRbaVV1oYgGD4HE4g=w768-h768-n-o-v1"
+        },
+        {
+          img:
+            "https://lh3.googleusercontent.com/bCDiJJkItiG4-s1ijDXxtsKy60hpf4Jo1fiQExKujtORmIrWgczZ67gLmFifzUuDxaRbaVV1oYgGD4HE4g=w768-h768-n-o-v1"
+        }
+      ],
+      eventsData: [
+        {
+          img:
+            "https://lh3.googleusercontent.com/bCDiJJkItiG4-s1ijDXxtsKy60hpf4Jo1fiQExKujtORmIrWgczZ67gLmFifzUuDxaRbaVV1oYgGD4HE4g=w768-h768-n-o-v1",
+          price: "free",
+          date: "12/2/2019",
+          eventTitle: "Mike Andrews",
+          discription:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi odio, molestiae minima minus impedit in tempore suscipit quia ullam quisquam recusandae. Quae eum expedita quam dolores consectetur fugiat, voluptatibus aspernatur."
+        },
+        {
+          img:
+            "https://lh3.googleusercontent.com/bCDiJJkItiG4-s1ijDXxtsKy60hpf4Jo1fiQExKujtORmIrWgczZ67gLmFifzUuDxaRbaVV1oYgGD4HE4g=w768-h768-n-o-v1",
+          price: "free",
+          date: "12/2/2019",
+          eventTitle: "Mike Andrews",
+          discription:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi odio, molestiae minima minus impedit in tempore suscipit quia ullam quisquam recusandae. Quae eum expedita quam dolores consectetur fugiat, voluptatibus aspernatur."
+        },
+        {
+          img:
+            "https://lh3.googleusercontent.com/bCDiJJkItiG4-s1ijDXxtsKy60hpf4Jo1fiQExKujtORmIrWgczZ67gLmFifzUuDxaRbaVV1oYgGD4HE4g=w768-h768-n-o-v1",
+          price: "free",
+          date: "12/2/2019",
+          eventTitle: "Mike Andrews",
+          discription:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi odio, molestiae minima minus impedit in tempore suscipit quia ullam quisquam recusandae. Quae eum expedita quam dolores consectetur fugiat, voluptatibus aspernatur."
+        },
+        {
+          img:
+            "https://lh3.googleusercontent.com/bCDiJJkItiG4-s1ijDXxtsKy60hpf4Jo1fiQExKujtORmIrWgczZ67gLmFifzUuDxaRbaVV1oYgGD4HE4g=w768-h768-n-o-v1",
+          price: "free",
+          date: "12/2/2019",
+          eventTitle: "Mike Andrews",
+          discription:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi odio, molestiae minima minus impedit in tempore suscipit quia ullam quisquam recusandae. Quae eum expedita quam dolores consectetur fugiat, voluptatibus aspernatur."
+        },
+        {
+          img:
+            "https://lh3.googleusercontent.com/bCDiJJkItiG4-s1ijDXxtsKy60hpf4Jo1fiQExKujtORmIrWgczZ67gLmFifzUuDxaRbaVV1oYgGD4HE4g=w768-h768-n-o-v1",
+          price: "free",
+          date: "12/2/2019",
+          eventTitle: "Mike Andrews",
+          discription:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi odio, molestiae minima minus impedit in tempore suscipit quia ullam quisquam recusandae. Quae eum expedita quam dolores consectetur fugiat, voluptatibus aspernatur."
+        },
+        {
+          img:
+            "https://lh3.googleusercontent.com/bCDiJJkItiG4-s1ijDXxtsKy60hpf4Jo1fiQExKujtORmIrWgczZ67gLmFifzUuDxaRbaVV1oYgGD4HE4g=w768-h768-n-o-v1",
+          price: "free",
+          date: "12/2/2019",
+          eventTitle: "Mike Andrews",
+          discription:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi odio, molestiae minima minus impedit in tempore suscipit quia ullam quisquam recusandae. Quae eum expedita quam dolores consectetur fugiat, voluptatibus aspernatur."
+        }
+      ],
       testimonialsData: [],
       editMode: false
     };
@@ -38,16 +134,27 @@ class App extends Component {
         <button className="EditMode-btn" onClick={this.toggleEditMode}>
           Edit Mode
         </button>
-        <LandingPage
-          editMode={editMode}
-          galleryData={galleryData}
-          eventsData={eventsData}
-          testimonialsData={testimonialsData}
-        />
+        <Switch>
+          <Route
+            path="/"
+            exact
+            render={() => (
+              <LandingPage
+                editMode={editMode}
+                galleryData={galleryData}
+                eventsData={eventsData}
+                testimonialsData={testimonialsData}
+              />
+            )}
+          />
+          <GalleryPage editMode={editMode} galleryData={galleryData} />
+          <EventPage />
+          <BlogPage />
+        </Switch>
         <Footer />
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
--- heaven_ecoland_front/src/Components/DefaultLanding/DefaultLanding.js
+++ heaven_ecoland_front/src/Components/DefaultLanding/DefaultLanding.js
@@ -11,119 +11,15 @@ import EventCard from "../EventCard/EventCard.js";
 import TestimonialCard from "../TestimonialCard/TestimonialCard.js";
 import OurStory from "../Our Story/OurStory.js";
 import ReacUs from "../Reach Us/ReachUs.js";
-import { tsConstructorType } from "@babel/types";
 //---------IMPORTED COMPONENTS--------------
 
 class DefaultLanding extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      data: [
-        {
-          img:
-            "https://lh3.googleusercontent.com/bCDiJJkItiG4-s1ijDXxtsKy60hpf4Jo1fiQExKujtORmIrWgczZ67gLmFifzUuDxaRbaVV1oYgGD4HE4g=w768-h768-n-o-v1",
-          price: "free",
-          date: "12/2/2019",
-          eventTitle: "Mike Andrews",
-          discription:
-            "Lorem ipsum dolor sLorem ipsum dolor sit amet conseLorem ipsum dolor sit amet conseLorem ipsum dolor sit amet conseLorem ipsum dolor sit amet conseLorem ipsum dolor sit amet conseit amet consectetur adipisicing elit. Commodi odio, molestiae minima minus impedit in tempore suscipit quia ullam quisquam recusandae. Quae eum expedita quam dolores consectetur fugiat, voluptatibus aspernatur."
-        },
-        {
-          img:
-            "https://lh3.googleusercontent.com/bCDiJJkItiG4-s1ijDXxtsKy60hpf4Jo1fiQExKujtORmIrWgczZ67gLmFifzUuDxaRbaVV1oYgGD4HE4g=w768-h768-n-o-v1",
-          price: "12",
-          date: "12/2/2019",
-          eventTitle: "Mike Andrews",
-          discription:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi odio, molestiae minima minus impedit in tempore suscipit quia ullam quisquam recusandae. Quae eum expedita quam dolores consectetur fugiat, voluptatibus aspernatur."
-        },
-        {
-          img:
-            "https://lh3.googleusercontent.com/bCDiJJkItiG4-s1ijDXxtsKy60hpf4Jo1fiQExKujtORmIrWgczZ67gLmFifzUuDxaRbaVV1oYgGD4HE4g=w768-h768-n-o-v1",
-          price: "15",
-          date: "12/2/2019",
-          eventTitle: "Mike Andrews",
-          discription:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi odio, molestiae minima minus impedit in tempore suscipit quia ullam quisquam recusandae. Quae eum expedita quam dolores consectetur fugiat, voluptatibus aspernatur."
-        },
-        {
-          img:
-            "https://lh3.googleusercontent.com/bCDiJJkItiG4-s1ijDXxtsKy60hpf4Jo1fiQExKujtORmIrWgczZ67gLmFifzUuDxaRbaVV1oYgGD4HE4g=w768-h768-n-o-v1",
-          price: "free",
-          date: "12/2/2019",
-          eventTitle: "Mike Andrews",
-          discription:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi odio, molestiae minima minus impedit in tempore suscipit quia ullam quisquam recusandae. Quae eum expedita quam dolores consectetur fugiat, voluptatibus aspernatur."
-        },
-        {
-          img:
-            "https://lh3.googleusercontent.com/bCDiJJkItiG4-s1ijDXxtsKy60hpf4Jo1fiQExKujtORmIrWgczZ67gLmFifzUuDxaRbaVV1oYgGD4HE4g=w768-h768-n-o-v1",
-          price: "free",
-          date: "12/2/2019",
-          eventTitle: "Mike Andrews",
-          discription:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi odio, molestiae minima minus impedit in tempore suscipit quia ullam quisquam recusandae. Quae eum expedita quam dolores consectetur fugiat, voluptatibus aspernatur."
-        },
-        {
-          img:
-            "https://lh3.googleusercontent.com/bCDiJJkItiG4-s1ijDXxtsKy60hpf4Jo1fiQExKujtORmIrWgczZ67gLmFifzUuDxaRbaVV1oYgGD4HE4g=w768-h768-n-o-v1",
-          price: "free",
-          date: "12/2/2019",
-          eventTitle: "Mike Andrews",
-          discription:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi odio, molestiae minima minus impedit in tempore suscipit quia ullam quisquam recusandae. Quae eum expedita quam dolores consectetur fugiat, voluptatibus aspernatur."
-        },
-        {
-          img:
-            "https://lh3.googleusercontent.com/bCDiJJkItiG4-s1ijDXxtsKy60hpf4Jo1fiQExKujtORmIrWgczZ67gLmFifzUuDxaRbaVV1oYgGD4HE4g=w768-h768-n-o-v1",
-          price: "free",
-          date: "12/2/2019",
-          eventTitle: "Mike Andrews",
-          discription:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi odio, molestiae minima minus impedit in tempore suscipit quia ullam quisquam recusandae. Quae eum expedita quam dolores consectetur fugiat, voluptatibus aspernatur."
-        },
-        {
-          img:
-            "https://lh3.googleusercontent.com/bCDiJJkItiG4-s1ijDXxtsKy60hpf4Jo1fiQExKujtORmIrWgczZ67gLmFifzUuDxaRbaVV1oYgGD4HE4g=w768-h768-n-o-v1",
-          price: "free",
-          date: "12/2/2019",
-          eventTitle: "Mike Andrews",
-          discription:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi odio, molestiae minima minus impedit in tempore suscipit quia ullam quisquam recusandae. Quae eum expedita quam dolores consectetur fugiat, voluptatibus aspernatur."
-        },
-        {
-          img:
-            "https://lh3.googleusercontent.com/bCDiJJkItiG4-s1ijDXxtsKy60hpf4Jo1fiQExKujtORmIrWgczZ67gLmFifzUuDxaRbaVV1oYgGD4HE4g=w768-h768-n-o-v1",
-          price: "free",
-          date: "12/2/2019",
-          eventTitle: "Mike Andrews",
-          discription:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi odio, molestiae minima minus impedit in tempore suscipit quia ullam quisquam recusandae. Quae eum expedita quam dolores consectetur fugiat, voluptatibus aspernatur."
-        },
-        {
-          img:
-            "https://lh3.googleusercontent.com/bCDiJJkItiG4-s1ijDXxtsKy60hpf4Jo1fiQExKujtORmIrWgczZ67gLmFifzUuDxaRbaVV1oYgGD4HE4g=w768-h768-n-o-v1",
-          price: "free",
-          date: "12/2/2019",
-          eventTitle: "Mike Andrews",
-          discription:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi odio, molestiae minima minus impedit in tempore suscipit quia ullam quisquam recusandae. Quae eum expedita quam dolores consectetur fugiat, voluptatibus aspernatur."
-        },
-        {
-          img:
-            "https://lh3.googleusercontent.com/bCDiJJkItiG4-s1ijDXxtsKy60hpf4Jo1fiQExKujtORmIrWgczZ67gLmFifzUuDxaRbaVV1oYgGD4HE4g=w768-h768-n-o-v1",
-          price: "free",
-          date: "12/2/2019",
-          eventTitle: "Mike Andrews",
-          discription:
-            "Lorem ipsum dolor  sit amet consectetur adipisicing elit. CLorem ipsum dolor sit amet consectetur adipisicing elit. Commodi odio, molestiaeommodi odio, molestiae minima minus impedit in tempore suscipit quia ullam quisquam recusandae. Quae eum expedita quam dolores consectetur fugiat, voluptatibus aspernatur."
-        }
-      ]
-    };
+    this.state = {};
   }
   render() {
-    const { data } = this.state;
-    const { editMode, ...rest } = this.props;
+    const { editMode, ...rest} = this.props;
     return (
       <div className="DefaultLanding">
         {/* -----------------HERO SECTION---------------- */}
@@ -141,7 +37,7 @@ class DefaultLanding extends Component {
               <h2>Gallery</h2>
             </div>
             <div className="images-container">
-              {data.map((gallery, i) => {
+              {rest.galleryData.map((gallery, i) => {
                 while (i < 9) {
                   return <GalleryCard srcImage={gallery.img} />;
                 }
@@ -156,7 +52,7 @@ class DefaultLanding extends Component {
               <h2>Events</h2>
             </div>
             <div className="event-container">
-              {data.map((event, i) => {
+              {rest.eventsData.map((event, i) => {
                 while (i < 6) {
                   return (
                     <EventCard
