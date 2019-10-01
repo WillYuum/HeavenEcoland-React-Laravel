06670fba47ddcb6e15664e1f04c2f18be7d6ee63
raed <testuser@example.com>
2019-10-01 10:58:19 +0300

    cors

--- heaven_ecoland_front/src/Components/GalleryCard/GalleryCard.js
+++ heaven_ecoland_front/src/Components/GalleryCard/GalleryCard.js
@@ -5,12 +5,13 @@ import "./GalleryCard.scss";
 // ----------SCSS--------------
 
 const GalleryCard = ({ srcImage }) => {
+  console.log("this is the srcImage");
   console.log(srcImage);
   return (
     <div className="image-card">
       <img
         className="galleryImage"
-        src={`http://127.0.0.1:8000/public/images/${srcImage}.png`}
+        src={`http://127.0.0.1:8000/images/${srcImage}`}
         crossOrigin="anonymous"
         alt="something went wrong"
       />
